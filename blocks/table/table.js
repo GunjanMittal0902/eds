96eec3a4bbd2eee9dc8b37eb73648ba63ec20d6b
GunjanMittal0902 <gunjanmittal0902@gmail.com>
2024-06-08 20:57:22 +0530

    Update table.js

--- blocks/table/table.js
+++ blocks/table/table.js
@@ -2,10 +2,8 @@ async function createTableHeader(table){
     let tr=document.createElement("tr");
     let sno=document.createElement("th");sno.appendChild(document.createTextNode("S.No"));
     let conuntry=document.createElement("th");conuntry.appendChild(document.createTextNode("Countries"));
-    let continent=document.createElement("th");continent.appendChild(document.createTextNode("Continent"));
     let capital=document.createElement("th");capital.appendChild(document.createTextNode("Capital"));
-    let abbr=document.createElement("th");abbr.appendChild(document.createTextNode("Abbreviation"));
-    tr.append(sno);tr.append(conuntry);tr.append(capital);tr.append(continent);tr.append(abbr);
+    tr.append(sno);tr.append(conuntry);tr.append(capital);
     table.append(tr);
 }
 async function createTableRow(table,row,i){
@@ -13,9 +11,7 @@ async function createTableRow(table,row,i){
     let sno=document.createElement("td");sno.appendChild(document.createTextNode(i));
     let conuntry=document.createElement("td");conuntry.appendChild(document.createTextNode(row.Country));
     let continent=document.createElement("td");continent.appendChild(document.createTextNode(row.Capital));
-    let capital=document.createElement("td");capital.appendChild(document.createTextNode(row.Continent));
-    let abbr=document.createElement("td");abbr.appendChild(document.createTextNode(row.Abbreviation));
-    tr.append(sno);tr.append(conuntry);tr.append(continent);tr.append(capital);tr.append(abbr);
+    tr.append(sno);tr.append(conuntry);tr.append(capital);
     table.append(tr);
 }
 
